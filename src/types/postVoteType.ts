6b36196932032d7872e544d69e5cbc31b9b93c78
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-05-31 19:31:18 -0400

    unused files

--- src/types/postVoteType.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const postVoteProps = z.object({
-  postID: z.string(),
-  voteCount: z.number(),
-  myCurrentVote: z.enum(["up", "down"]).nullable(),
-});
-
-export type PostVoteType = z.infer<typeof postVoteProps>;
--- src/types/stateVoteType.ts
+++
@@ -1,2 +0,0 @@
-export type VoteType = "up" | "down";
-export type CurrentVoteType = VoteType | null;
